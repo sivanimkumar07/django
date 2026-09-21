b96972b51f0296e3eb2e700d5e822e20c6bb6d9f
sivani <sivanimkumar7@gmail.com>
2026-09-21 13:02:44 +0530

    Add students app with models, forms, views, templates, and URL configurations for student management

---
+++ school_project/students/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('', views.home, name='home'),
+
+    path(
+        'add/',
+        views.add_student,
+        name='add_student'
+    ),
+
+    path(
+        'edit/<int:id>/',
+        views.edit_student,
+        name='edit_student'
+    ),
+
+    path(
+        'delete/<int:id>/',
+        views.delete_student,
+        name='delete_student'
+    ),
+]
